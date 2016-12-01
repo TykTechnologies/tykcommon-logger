f9ddf36d348d3a5c5f39f1d44068a653de09cb1c
Martin Buhr <martin@tyk.io>
2016-12-01 16:39:24 +0000

    Changed dependency

--- init.go
+++ init.go
@@ -1,10 +1,11 @@
 package logger
 
 import (
-	"github.com/sirupsen/logrus"
-	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 	"os"
 	"strings"
+
+	prefixed "github.com/TykTechnologies/logrus-prefixed-formatter"
+	"github.com/sirupsen/logrus"
 )
 
 var log = logrus.New()
